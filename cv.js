1dae4184a8a546ae701dcba77f5a81348b2f1f6a
Tomasl77 <lobgeois.tomas@free.fr>
2018-05-18 12:04:07 +0200

    modification background + images

--- cv.js
+++ cv.js
@@ -1,3 +1,7 @@
+
+var plz = '<div id="color"><br />Veuillez passer votre souris sur l\'une des images s\'il vous plait</div>';
+
+
 function writetext(txt) {
     document.getElementById('title').innerHTML = txt;
     document.getElementById('title').style.backgroundColor = 'yellow';
@@ -6,6 +10,7 @@ function writetext(txt) {
 
 function mouseover(){
 	document.getElementById('title').style.backgroundColor = 'transparent';
+	document.getElementById('corps').innerHTML = plz;
 }
 function writecorps(txt){
 	document.getElementById('corps').innerHTML = txt;
